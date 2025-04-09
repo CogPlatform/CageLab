578e784076a4c68887174edda25ad5ceca3788a6
HuYang <hy.ccccr@gmail.com>
2025-04-10 02:35:42 +0800

    chore: :construction: init doc feature

---
+++ doc/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { useColorMode } from '@vueuse/core'
+
+useColorMode('auto')
+</script>
+
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+    </main>
+  </SidebarProvider>
+</template>

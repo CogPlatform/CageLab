42fb5480492637160ed2490936fbfa1afd46128d
iandol <iandol@cog5.org>
2025-11-04 14:40:54 +0800

    try to fix rogue cogmoteGO services

--- software/scripts/cagelab-reset-services.sh
+++ software/scripts/cagelab-reset-services.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 # a script to reset all cage lab services to their default states
 # should be run after any changes to the services or their configurations
+echo "Resetting Cage Lab services to default states..."
 
+# try to ensure no system services are running that might interfere
+sudo systemctl disable --now cogmoteGO.service > /dev/null 2>&1
+sudo systemctl disable --now theConductor.service  > /dev/null 2>&1
+sudo systemctl disable --now mediamtx.service  > /dev/null 2>&1
+sudo systemctl disable --now obs.service  > /dev/null 2>&1
+
+# user services reset
 cd "$HOME/.config/systemd/user" || return
 systemctl --user disable cogmoteGO.service
 rm -f cogmoteGO.service
@@ -17,8 +25,7 @@ for s in $sl; do
 		[[ -d "/usr/local/MATLAB/R2025b" ]] && ln -sfv "$HOME/Code/CageLab/software/services/theConductor2025b.dservice" "$HOME/.config/systemd/user/theConductor.service"
 	fi
 	systemctl --user daemon-reload
-	systemctl --user enable $s
+	systemctl --user enable --now $s
+	printf "...Reset and enabled %s\n" "$s"
 done
-sleep 1
-systemctl --user daemon-reload
-systemctl --user restart mediamtx cogmoteGO theConductor obs obs-fix
+echo "All cage lab services have been reset to their default states."
